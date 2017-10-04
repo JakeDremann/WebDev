f6579e93b3c7ca40d28d71d1d7af437d1084b26f
Jake Dremann <jgdremann@gmail.com>
2017-10-04 02:16:44 +0000

    css and home,about,help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
   resources :microposts
   resources :users
+  root 'my_pages#home'
   root 'users#index'
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
